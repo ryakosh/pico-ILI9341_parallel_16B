66e1bc1d5d1265e470649829e0f3c463fb8d2a23
ryakosh <ryakosh@gmail.com>
2021-04-27 19:17:25 +0430

    Add more primary colors

--- defines.h
+++ defines.h
@@ -2,7 +2,25 @@
 #define DEFINES_H
 
 // Primary Colors
-#define COLOR_RED 0xF800
+#define COLOR_BLACK       0x0000 
+#define COLOR_NAVY        0x000F
+#define COLOR_DARKGREEN   0x03E0
+#define COLOR_DARKCYAN    0x03EF      
+#define COLOR_MAROON      0x7800      
+#define COLOR_PURPLE      0x780F  
+#define COLOR_OLIVE       0x7BE0     
+#define COLOR_LIGHTGREY   0xC618     
+#define COLOR_DARKGREY    0x7BEF      
+#define COLOR_BLUE        0x001F      
+#define COLOR_GREEN       0x07E0      
+#define COLOR_CYAN        0x07FF     
+#define COLOR_RED         0xF800      
+#define COLOR_MAGENTA     0xF81F      
+#define COLOR_YELLOW      0xFFE0     
+#define COLOR_WHITE       0xFFFF    
+#define COLOR_ORANGE      0xFD20     
+#define COLOR_GREENYELLOW 0xAFE5 
+#define COLOR_PINK        0xF81F
 
 #define LCD_PINS_MASK 0x7CFFFF
 #define LCD_PINS_8B 0xFF
